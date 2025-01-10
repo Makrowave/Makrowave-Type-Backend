5f8c242d17aed14ab87a71b2f3174f7135d4ed3f
makrowave <maksstyzej@gmail.com>
2025-01-10 16:52:14 +0100

    fix: theme validation object cast

--- Makrowave-Type-Backend/Validation/ColorListAttribute.cs
+++ Makrowave-Type-Backend/Validation/ColorListAttribute.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
+using Makrowave_Type_Backend.Dtos;
 
 namespace Makrowave_Type_Backend.Validation;
 
@@ -18,7 +19,8 @@ public class ColorListAttribute : ValidationAttribute
 
     protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
     {
-        var colorList = (List<string>)validationContext.ObjectInstance;
+        var userTheme = (UserThemeDto)validationContext.ObjectInstance;
+        var colorList = userTheme.Gradient;
         if (colorList.Count < 2 || colorList.Count > 32767)
         {
             return new ValidationResult(GetCountErrorMessage());
