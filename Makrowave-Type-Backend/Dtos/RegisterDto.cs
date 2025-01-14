9dc7c76cbe8e51ba2d31e4f8f4c854b89569c5cb
makrowave <maksstyzej@gmail.com>
2025-01-14 12:56:52 +0100

    fix: validation fix and gitignore fix

--- Makrowave-Type-Backend/Dtos/RegisterDto.cs
+++ Makrowave-Type-Backend/Dtos/RegisterDto.cs
@@ -6,10 +6,10 @@ namespace Makrowave_Type_Backend.Dtos;
 public class RegisterDto
 {
     [Required(ErrorMessage = "Username is required")]
-    [RegularExpression(Regexes.Name, ErrorMessage = "Invalid username format")]
+    [RegularExpression(Regexes.Name, ErrorMessage = "Invalid username")]
     public string Username { get; set; } = null!;
 
     [Required(ErrorMessage = "Password is required")]
-    [RegularExpression(Regexes.Password, ErrorMessage = "Invalid password format")]
+    [RegularExpression(Regexes.Password, ErrorMessage = "Invalid password")]
     public string Password { get; set; } = null!;
 }
--- Makrowave-Type-Backend/Dtos/UsernameDto.cs
+++ Makrowave-Type-Backend/Dtos/UsernameDto.cs
@@ -1,8 +1,11 @@
 using System.ComponentModel.DataAnnotations;
+using Makrowave_Type_Backend.Validation;
 
 namespace Makrowave_Type_Backend.Dtos;
 
 public class UsernameDto
 {
-    [Required] public string Username { get; set; } = null!;
+    [Required] 
+    [RegularExpression(Regexes.Name, ErrorMessage = "Invalid username")]
+    public string Username { get; set; } = null!;
 }
